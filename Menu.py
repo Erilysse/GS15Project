8d6d54bfbcca1c66d1f116136a07685d6490bbc4
Erilysse <estelle.manic@gmail.com>
2019-11-20 11:38:49 +0100

    New branch
    New branch

--- Menu.py
+++ Menu.py
@@ -1,3 +1,5 @@
+import switch as switch
+
 print("Bienvenue dans le Menu")
 print("Choisir une option : \n"
       "-1 : Générer des couples de clés publiques / privées \n"
@@ -13,3 +15,21 @@ print("Choisir une option : \n"
 option = input("Indiquez l'option : ")
 option = int(option)
 print("Vous avez choisi l'option", option)
+switch (option){
+      case 1:  option = "-1";
+            break;
+      case 2: option = "0";
+            break;
+      case 3: option = "1";
+            break;
+      case 4: option = "2";
+            break;
+      case 5: option = "3";
+            break;
+      case 6: option = "4";
+            break;
+      case 7: option = "5";
+            break;
+      case 8: option = "6";
+            break;
+}
